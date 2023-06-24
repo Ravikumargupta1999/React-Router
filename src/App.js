96c1bd9d0cbe2d458df18b82374f9a59c4cfdee8
unknown <ravikumargupta391999@gmail.com>
2023-06-24 19:50:52 +0530

    Dynamic Routing

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import About from "./pages/About"
 import Home from "./pages/Home"
 import Items from "./pages/Items"
 import Navbar from "./components/Navbar";
-
+import ItemDetails from "./pages/ItemDetails";
 function App() {
   // Second way to create route
   // const routes = createRoutesFromElements(
@@ -24,13 +24,17 @@ function App() {
   // First way to create route
   const router = createBrowserRouter([
     { 
-      path :'/root',
+      path :'/',
       element :<Navbar/>,
       children :[
         // {path : "", element : <Home/>},
         {index : true, element : <Home/>},
         {path : "about", element :<About/>},
-        {path : "items", element :<Items/>}
+        {path : "items", element :<Items/>},
+        {
+           path : 'items/:id',
+           element : <ItemDetails/>
+        }
     ]},
   ])
 
--- src/pages/Items.js
+++ src/pages/Items.js
@@ -1,13 +1,26 @@
 import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import ItemDetails from "./ItemDetails";
 function Items(){
     return(
         <> 
         
            <main>
                <h1>Items Page</h1>
-               
            </main>
+           <ul>
+              <Link to = "/items/item-1">
+                  <li>Item-1</li>
+              </Link>
+
+              <Link to = "/items/item-2">
+                  <li>Item-2</li>
+              </Link>
+
+              <Link to = "/items/item-3">
+                  <li>Item-3</li>
+              </Link>
+           </ul>
         </>
     )
 }
