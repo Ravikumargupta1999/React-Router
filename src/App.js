752c5bca837d5be484b74b93b9b4b7f39624187c
unknown <ravikumargupta391999@gmail.com>
2023-06-24 17:11:19 +0530

    Route Element

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import { createBrowserRouter,RouterProvider } from "react-router-dom";
+import { createBrowserRouter,RouterProvider,createRoutesFromElements,Route } from "react-router-dom";
 
 import About from "./pages/About"
 import Home from "./pages/Home"
 import Items from "./pages/Items"
 
 function App() {
-  const router = createBrowserRouter([
-    {path : "/", element : <Home/>},
-    {path : "/about", element :<About/>},
-    {path : "/items", element :<Items/>},
-  ])
+  // Second way to create route
+  const routes = createRoutesFromElements(
+    <> 
+       <Route path = "/" element = {<Home/>}/>
+       <Route path = "/about" element = {<About/>}/>
+       <Route path = "/items" element = {<Items/>}/>
+    </>
+  );
+  const router = createBrowserRouter(routes);
+
+
+  // First way to create route
+  // const router = createBrowserRouter([
+  //   {path : "/", element : <Home/>},
+  //   {path : "/about", element :<About/>},
+  //   {path : "/items", element :<Items/>},
+  // ])
+
+
   return (
 
     <>
